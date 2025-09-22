960fb802b9c878af67b93948db16ba6b862032e9
VOsipenkov <jumpViaOcean@gmail.com>
2025-09-22 22:38:11 +0300

    WhoAmI component with state

--- src/index.js
+++ src/index.js
@@ -1,13 +1,48 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/app';
 import reportWebVitals from './reportWebVitals';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
+class WhoAmI extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      years: 26
+    }
+    this.nextYear = this.nextYear.bind(this);
+  }
+
+  nextYear(){
+    this.setState(state=>({
+      years: ++state.years
+    }));
+  }
+
+  render(){
+        const {name, surname, link} = this.props;
+        const {years} = this.state;
+        return (<React.Fragment>
+          <button onClick={this.nextYear}>++</button>
+          <h1>My name is {name}  and surename {surname}, age {years} </h1>
+          <a href={link}>My profile</a>
+        </React.Fragment>);
+  }
+}
+
+
+const All = () => {
+ return (<>  
+  <WhoAmI name="user1" surname="surname1" link="facebook.com" />
+  <WhoAmI name="user2" surname="surname2" link="vk.com" />
+  <WhoAmI name="user3" surname="surname3" link="google.com" />
+</>);
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <All/>
   </React.StrictMode>
 );
